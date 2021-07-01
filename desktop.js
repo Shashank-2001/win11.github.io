b5e6eca6d49500eeb3fce7a14724618ac2918f48
shashank <vishwakarma.shashank2001@gmail.com>
2021-07-01 12:27:20 +0530

    win 11 1.0.0

---
+++ desktop.js
@@ -0,0 +1,14 @@
+
+document.getElementById("winlogo").addEventListener("click",()=>{
+    if(document.getElementById("content").style.bottom== '-500px'){
+document.getElementById("content").style.bottom='50px';
+
+    }
+   else{
+
+        document.getElementById("content").style.bottom='-500px';
+        
+    }    
+    
+
+})
